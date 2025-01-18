e9b9161fbc1e35f5489d11bec0f2c48b1d048885
latharsanjeet12 <39617468+latharsanjeet12@users.noreply.github.com>
2025-01-18 19:34:29 +0530

    Add files via upload

---
+++ a.py
@@ -0,0 +1,28 @@
+#https://www.w3schools.com/python/python_lists_remove.asp
+# a Python object (dict):
+
+class e:
+    #v="hello"
+    def __str__(self):
+        return (self.v)
+    def __init__(self):
+        self.v="[2,4,5,6,8,3,9,5,7,8,8,0]"    
+    def s(self):
+        print(self.v[3:6])
+class b:
+    def __str__(self):
+        return ("class B")
+    @classmethod    
+    def now(cls):
+        m=e()
+        ss="tttttt"
+        return m
+        
+        
+c=b.now()
+print(c)
+c.s()
+
+print(complex(3,8)+complex(4,6))
+
+
